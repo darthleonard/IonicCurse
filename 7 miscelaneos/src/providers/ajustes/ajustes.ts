f77c9eb1bc5a308bd4d44832613715e3c2ffb8dd
darthleonard <leolinuxmx@gmail.com>
2020-05-30 00:55:04 -0500

    107 grabar y leer storage nativo del dispositivo

--- 7 miscelaneos/src/providers/ajustes/ajustes.ts
+++ 7 miscelaneos/src/providers/ajustes/ajustes.ts
@@ -16,7 +16,17 @@ export class AjustesService {
   cargarStorage() : Promise<any> {
     let promesa = new Promise((resolve, reject) => {
       if(this.platform.is("cordova")) {
-      
+        console.log("inicializando storage");
+        this.storage.ready().then(() => {
+          console.log("storage listo");
+          this.storage.get("ajustes").then(ajustes => {
+            if(ajustes) {
+              this.ajustes = ajustes;
+            }
+            
+            resolve();
+          });
+        })
       } else {
         if(localStorage.getItem("ajustes")) {
           this.ajustes = JSON.parse(localStorage.getItem("ajustes"));
@@ -29,7 +39,9 @@ export class AjustesService {
 
   guardarStorage() {
     if(this.platform.is("cordova")) {
-      
+      this.storage.ready().then(() => {
+        this.storage.set("ajustes", this.ajustes);
+      });
     } else {
       localStorage.setItem("ajustes", JSON.stringify(this.ajustes));
     }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IonicCurse
 Curso de ionic 3
+
+$ export ANDROID_HOME=/android-sdk
+$ export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+$ echo $ANDROID_HOME
+
