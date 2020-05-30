d03aa131371a1ac34ce6e4d6dabf6831f23a5213
darthleonard <leolinuxmx@gmail.com>
2020-05-29 23:25:17 -0500

    103 demostracion del ciclo de vida de una pagina

--- 7 miscelaneos/src/pages/pagina2/pagina2.ts
+++ 7 miscelaneos/src/pages/pagina2/pagina2.ts
@@ -11,6 +11,45 @@ export class Pagina2Page {
     public navParams: NavParams) {
   }
 
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad");
+  }
+
+  ionViewWillEnter() {
+    console.log("ionViewWillEnter");
+  }
+
+  ionViewDidEnter() {
+    console.log("ionViewDidEnter");
+  }
+
+  ionViewWillLeave() {
+    console.log("ionViewWillLeave");
+  }
+
+  ionViewDidLeave() {
+    console.log("ionViewDidLeave");
+  }
+
+  ionViewWillUnload() {
+    console.log("ionViewWillUnload");
+  }
+
+  ionViewCanEnter() {
+    console.log("ionViewCanEnter");
+    let numero = Math.round(Math.random() * 10);
+    console.log(numero);
+    return numero >= 3;
+  }
+
+  ionViewCanLeave() {
+    console.log("ionViewCanLeave");
+    let promesa = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(true), 2000);
+    });
+    return promesa;
+  }
+
   irPagina3() {
     this.navCtrl.push("mi-pagina3");
   }
